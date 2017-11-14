d4ef77bd9519e4456822742c19ea331acc275a42
kacky24 <kakiuchi@crimson.q.t.u-tokyo.ac.jp>
2017-11-15 00:27:24 +0900

    create data_loader

---
+++ data_loader.py
@@ -0,0 +1,144 @@
+import os
+import re
+import pickle
+import nltk
+import skimage.io, skimage.transform
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from build_vocab import Vocab
+from constant import get_symbol_id
+
+
+class Flickr7kDataset(Dataset):
+    '''Flickr7k dataset'''
+    def __init__(self, img_dir, caption_file, vocab, transform=None):
+        '''
+        Args:
+            data_dir: Direcutory with all the images
+            caption_file: Path to the factual caption file 
+            vocab: Vocab instance
+            transform: Optional transform to be applied
+        '''
+        self.img_dir = img_dir
+        self.imgname_caption_list = self._get_imgname_and_caption(caption_file)
+        self.vocab = vocab
+        self.transform = transform
+
+    def _get_imgname_and_caption(self, caption_file):
+        '''extract image name and caption from factual caption file'''
+        with open(caption_file, 'r') as f:
+            res = f.readlines()
+
+        imgname_caption_list = []
+        r = re.compile(r'#\d*')
+        for line in res:
+            img_and_cap = r.split(line)
+            img_and_cap = [x.strip() for x in img_and_cap]
+            imgname_caption_list.append(img_and_cap)
+
+        return imgname_caption_list
+
+    def __len__(self):
+        return len(self.imgname_caption_list)
+
+    def __getitem__(self, ix):
+        '''return one data pair (image and captioin)'''
+        img_name = self.imgname_caption_list[ix][0]
+        img_name = os.path.join(self.img_dir, img_name)
+        caption = self.imgname_caption_list[ix][1]
+        
+        image = skimage.io.imread(img_name)
+        if self.transform is not None:
+            image = self.transform(image)
+        
+        # convert caption to word ids
+        r = re.compile("\.")
+        tokens = nltk.tokenize.word_tokenize(r.sub("", caption).lower())
+        caption = []
+        caption.append(self.vocab('<s>'))
+        caption.extend([self.vocab(token) for token in tokens])
+        caption.append(self.vocab('</s>'))
+        caption = torch.Tensor(caption)
+        return image, caption
+
+
+def get_data_loader(img_dir, caption_file, vocab, batch_size, transform=None, shuffle=False, num_workers=0):
+    '''Return data_loader'''
+    if transform is None:
+        transform = transforms.Compose([
+            Rescale((224, 224)),
+            transforms.ToTensor()
+            ])
+
+    flickr7k = Flickr7kDataset(img_dir, caption_file, vocab, transform)
+
+    data_loader = DataLoader(dataset=flickr7k,
+                             batch_size=batch_size,
+                             shuffle=shuffle,
+                             num_workers=num_workers,
+                             collate_fn=collate_fn)
+    return data_loader
+    
+
+class Rescale:
+    '''Rescale the image to a given size
+    Args:
+        output_size(int or tuple)
+    '''
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        self.output_size = output_size
+
+    def __call__(self, image):
+        h, w = image.shape[:2]
+        if isinstance(self.output_size, int):
+            if h > w:
+                new_h, new_w = self.output_size * h / w, self.output_size
+            else:
+                new_h, new_w = self.output_size, self.output_size * w / h
+        else:
+            new_h, new_w = self.output_size
+
+        new_h, new_w = int(new_h), int(new_w)
+        image = skimage.transform.resize(image, (new_h, new_w))
+
+        return image
+
+
+
+def collate_fn(data):
+    '''create minibatch tensors from data(list of tuple(image, caption))'''
+    data.sort(key=lambda x:len(x[1]), reverse=True)
+    images, captions = zip(*data)
+    
+    # images : tuple of 3D tensor -> 4D tensor
+    images = torch.stack(images, 0)
+
+    # captions : tuple of 1D Tensor -> 2D tensor
+    lengths = [len(cap) for cap in captions]
+    captions = [pad_sequence(cap, max(lengths)) for cap in captions]
+    captions = torch.stack(captions, 0)
+
+    return images, captions, lengths
+
+
+def pad_sequence(seq, max_len):
+    seq = torch.cat((seq, torch.zeros(max_len - len(seq))))
+    return seq
+    
+
+if __name__ == "__main__":
+    with open("data/vocab.pkl", 'rb') as f:
+        vocab = pickle.load(f)
+
+    data_loader = get_data_loader("data/flickr7k_images", "data/factual_train.txt", vocab, 3)
+
+    for i, (images, captions, lengths) in enumerate(data_loader):
+        print(i)
+        print(images.shape)
+        print(captions)
+        print(lengths)
+        print()
+        if i == 3:
+            break
